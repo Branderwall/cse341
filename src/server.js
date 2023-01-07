9838ad7576155c1f87fd9c0b458d785ecaa3d890
Adam <tttgkm@gmail.com>
2023-01-07 13:16:32 -0800

    change port number to 3000

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 // create port variable
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 // Send request to routes for directing
 app.use("/", require("./routes"));
