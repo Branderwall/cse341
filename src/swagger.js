a05805e2b89d7791695772768ef6ccf69779e488
Adam <tttgkm@gmail.com>
2023-01-28 10:58:04 -0800

    Swagger Schema update

--- src/swagger.js
+++ src/swagger.js
@@ -1,16 +1,63 @@
-const swaggerAutogen = require('swagger-autogen')();
+const swaggerAutogen = require("swagger-autogen")();
 
-const doc = {
-  info: {
-    title: 'Contacts API',
-    description: 'Contact creation, retrieval, update, and deletion API',
-  },
-  host: 'adamcse341.onrender.com',
-  schemes: ['https'],
+const contactSchema = {
+    Contact: {
+        type: "object",
+        properties: {
+            id: { type: "ObjectId" },
+            firstName: {
+                type: "string",
+                example: "John",
+            },
+            lastName: {
+                type: "string",
+                example: "Smith",
+            },
+            email: {
+                type: "string",
+                example: "jsmith@test.com",
+            },
+            favoriteColor: {
+                type: "string",
+                example: "Blue",
+            },
+            birthday: {
+                type: "string",
+                example: "01-01-2001",
+            },
+        },
+    },
 };
 
-const outputFile = './swagger.json';
-const endpointsFiles = ['./routes/index.js'];
+const doc = {
+    info: {
+        title: "Contacts API",
+        description: "Contact creation, retrieval, update, and deletion API",
+    },
+    servers: [{ url: "adamcse341.onrender.com" }, { url: "localhost:3000" }],
+    schemes: ["https", "http"],
+    // tags: [
+    //     {
+    //         name: "Contacts",
+    //         description: "Contact information",
+    //     },
+    // ],
+    components: {
+        schemas: contactSchema,
+    },
+    definitions: {
+        Contacts: {
+            firstName: "John",
+            lastName: "Smith",
+            email: "jsmith@test.com",
+            favoriteColor: "Blue",
+            birthday: "01-01-2001",
+        },
+    },
+};
+
+const outputFile = "./swagger.json";
+const endpointsFiles = ["./routes/index.js"];
 
 /* NOTE: if you use the express Router, you must pass in the 
    'endpointsFiles' only the root file where the route starts,
