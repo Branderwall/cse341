d2ba8f5b3b3bd36ddcb2ed6dc6626a0d3d53c3c4
Adam <tttgkm@gmail.com>
2023-01-28 14:59:04 -0800

    Finalize Lesson 4

--- src/swagger.js
+++ src/swagger.js
@@ -1,37 +1,5 @@
 const swaggerAutogen = require("swagger-autogen")();
 
-// const contactSchema = {
-//     Contacts: {
-//         type: "object",
-//         properties: {
-//             id: { type: "ObjectId" },
-//             firstName: {
-//                 type: "string",
-//                 example: "John",
-//             },
-//             lastName: {
-//                 type: "string",
-//                 example: "Smith",
-//             },
-//             email: {
-//                 type: "string",
-//                 example: "jsmith@test.com",
-//             },
-//             favoriteColor: {
-//                 type: "string",
-//                 example: "Blue",
-//             },
-//             birthday: {
-//                 type: "string",
-//                 example: "01-01-2001",
-//             },
-//         },
-//     },
-// };
-// components: {
-//   schemas: contactSchema,
-// },
-
 // localhost:3000
 // adamcse341.onrender.com
 const doc = {
