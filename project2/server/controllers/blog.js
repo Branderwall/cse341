12b8ab9171dac6aa7cd3414021b396ed80e2ac51
Adam <tttgkm@gmail.com>
2023-02-02 22:31:37 -0800

    Create and Delete Post

--- project2/server/controllers/blog.js
+++ project2/server/controllers/blog.js
@@ -36,7 +36,16 @@ const getPostById = async (req, res) => {
 const createPost = async (req, res) => {
     // #swagger.tags = ['Blog']
     try {
-        const blog = req.body;
+        // const blog = req.body;
+        const blog = {
+            title: req.body.title,
+            slug: req.body.slug,
+            body: req.body.body,
+            author: req.body.author,
+            publishDate: req.body.publishDate,
+            tags: req.body.tags,
+            featuredImage: req.body.featuredImage,
+        };
 
         const result = await mongodb
             .getDB()
@@ -52,8 +61,32 @@ const createPost = async (req, res) => {
     }
 };
 
+const deletePost = async (req, res) => {
+    // #swagger.tags = ['Blog']
+    try {
+        const id = req.params.id;
+        const filter = {
+            _id: new ObjectId(id),
+        };
+
+        const result = await mongodb
+            .getDB()
+            .db("cse341")
+            .collection("blog")
+            .deleteOne(filter);
+
+        if (result.deletedCount > 0) {
+            res.status(200).send();
+        }
+    } catch (err) {
+        // console.log(err);
+        res.json({ message: err });
+    }
+};
+
 module.exports = {
     getAllPosts,
     getPostById,
     createPost,
+    deletePost,
 };
--- project2/server/routes/blog.js
+++ project2/server/routes/blog.js
@@ -3,6 +3,7 @@ const controller = require("../controllers");
 
 router.get("/", controller.blog.getAllPosts);
 router.get("/:id", controller.blog.getPostById);
-router.post("/new", controller.blog.createPost);
+router.post("/", controller.blog.createPost);
+router.delete("/:id", controller.blog.deletePost);
 
 module.exports = router;
